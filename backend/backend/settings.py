1cbb355cd56f91448673db4f763edeb00a430ee5
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-10-05 12:02:25 +0100

    backend render set-up

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zxl08g@%2k)usio05b#2ths*59fz2(wp(fmplkhi9)!_&j(_56'
+SECRET_KEY = os.environ.get('PRUDENTIA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1','localhost','prudentiaapi.onrender.com'
+]
 
 
 # Application definition
@@ -86,13 +88,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASE_URL = os.environ.get('PRUDENTIA_DATABASE_URL')
 
