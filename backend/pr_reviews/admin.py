b60384460e663929c3bdd5db73ae73801709d077
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-10-07 19:51:03 +0100

    registered pr-reviews with admin got backend protection working

--- backend/pr_reviews/admin.py
+++ backend/pr_reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import PRReviewTable, PRReviewMeasure
 
-# Register your models here.
+# Register the PRReviewTable model
+class PRReviewTableAdmin(admin.ModelAdmin):
+    list_display = ['returnId', 'created_at', 'updated_at']
+
+# Register the PRReviewMeasure model
+class PRReviewMeasureAdmin(admin.ModelAdmin):
+    list_display = ['pr_review', 'source', 'measure', 'current_Q0', 'average']
+
+# Register the models in the admin interface
+admin.site.register(PRReviewTable, PRReviewTableAdmin)
+admin.site.register(PRReviewMeasure, PRReviewMeasureAdmin)
