bc327f3ea97e3f4e70f56857345fd807439313f6
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-10-08 13:19:22 +0100

    initial dev set-up

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('PRUDENTIA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-#FRONTEND_URL = 'http://localhost:3000/'
-FRONTEND_URL = 'https://prudentia.onrender.com/'
+FRONTEND_URL = 'http://localhost:3000/'
+#FRONTEND_URL = 'https://prudentia.onrender.com/'
 
 ALLOWED_HOSTS = [
     '127.0.0.1','localhost','prudentiaapi.onrender.com'
